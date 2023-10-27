5dc81a9989a381b707a93d57070bfbd7de8183e3
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-10-27 17:53:09 +0200

    fixed issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './index.css';
 import App from './App';
 import client from '../src/apollo/client';
 import { ApolloProvider } from '@apollo/client';
